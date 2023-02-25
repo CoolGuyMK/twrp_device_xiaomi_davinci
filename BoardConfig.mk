ef456c32426832cfe9f1ed572b9f8a4bac14270a
CoolGuyMK <coolguymk1@gmail.com>
2023-02-25 01:34:53 +0000

    davinci: adapt for pb

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -129,6 +129,10 @@ TW_EXCLUDE_TWRPAPP := true
 TW_OVERRIDE_SYSTEM_PROPS := \
     "ro.build.date.utc;ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
 
+# PitchBlack specific build flags
+PB_DEVICE_RATIO := "19.5:9"
+PB_TORCH_PATH := "/sys/class/leds/led:torch_0"
+
 BOARD_USES_METADATA_PARTITION := true
 PLATFORM_VERSION := 16.1.0
 PLATFORM_VERSION_LAST_STABLE := $(PLATFORM_VERSION)
--- twrp_davinci.mk
+++ twrp_davinci.mk
@@ -20,7 +20,7 @@ PRODUCT_RELEASE_NAME := davinci
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := davinci
